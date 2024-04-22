e09c6aeb1eb95aee232753fc0e4db2002ffe6746
mohammadhoseinrs <mohammadhoseinr1377@gmail.com>
2024-04-22 15:14:27 +0200

    deploy

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 4000;
+const MONGOURL = process.env.MONGO_URL || "mongodb://localhost:27017/ecommerce";
 
 const connectToDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL);
+    await mongoose.connect(MONGOURL);
     console.log("database is connected");
   } catch (err) {
     console.log(err);
