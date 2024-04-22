c9f0abd91ec59800cc1f4405d75cbdaf8880d6ac
mohammadhoseinrs <mohammadhoseinr1377@gmail.com>
2024-04-22 15:05:43 +0200

    test

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,23 @@
+const userModel = require("./../models/user");
+
 exports.register = async (req, res) => {
-  return res.json({ message: "register" });
+  try {
+    const { fullName, email, password, confirmPassword } = req.body;
+
+    const newUser = await userModel.create({
+      fullName,
+      email,
+      password,
+    });
+
+    return res.json({ newUser });
+  } catch (err) {
+    console.log(err);
+  }
 };
 exports.login = async (req, res) => {
-  return res.json({ message: "register" });
+  const allUser = await userModel.find({});
+  return res.json({ allUser });
 };
 exports.getMe = async (req, res) => {
   return res.json({ message: "register" });
--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,7 @@ const authController=require('./../controllers/auth')
 const router=express.Router()
 
 
-router.route('/register').get(authController.register)
+router.route('/register').post(authController.register)
+router.route('/login').get(authController.login)
 
 module.exports=router
