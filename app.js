b5edff988b9c66c0e13471410b7ab5e10cef7d6f
mohammadhoseinrs <mohammadhoseinr1377@gmail.com>
2024-04-25 11:27:48 +0200

    developing register api

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
 const express = require("express");
 const cors = require("cors");
 
-const authRouter=require('./routes/auth')
-
-
+const authRouter = require("./routes/auth");
 
 const app = express();
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "DELETE, PUT");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  if ("OPTIONS" == req.method) {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-
-
-app.use('/auth',authRouter)
-module.exports=app
+app.use("/auth", authRouter);
+module.exports = app;
